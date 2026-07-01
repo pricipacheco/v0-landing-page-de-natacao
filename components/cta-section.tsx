cf14cdb23d12b39637bcc5eceba4bb7c162b8a5a
pricipacheco <prici.pacheco@gmail.com>
2026-07-01 15:45:30 -0300

    Remove animate-wave class from SVG in CTA section
    Removed the 'animate-wave' class from the SVG element.

--- components/cta-section.tsx
+++ components/cta-section.tsx
@@ -26,7 +26,7 @@ export function CTASection() {
       {/* Wave pattern */}
       <div className="absolute bottom-0 left-0 right-0 h-24 overflow-hidden">
         <svg 
-          className="absolute bottom-0 w-[200%] h-full animate-wave opacity-20"
+          className="absolute bottom-0 w-[200%] h-full opacity-20"
           viewBox="0 0 1440 120" 
           preserveAspectRatio="none"
         >
